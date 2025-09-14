ecceacad6f3ffea2363ac03a4bd706145d7f299e
zach.duan <zach.duan@outlook.com>
2025-09-14 13:54:30 +0000

    [feature] add demo show

---
+++ eval/aes/show_demo.py
@@ -0,0 +1,66 @@
+import json
+import os
+import random
+from PIL import Image
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+bench_json = "data/sft_data/AesEditor/data_json/aes_edit_test.jsonl"
+data_dir = "data/sft_data/AesEditor/"
+result_dirs = {
+    "Bagel": "results/aes_eval/aes_edit_bagel/edited_images",
+    "Bagel_14k": "results/aes_eval/aes_edit_bagel_14/edited_images"
+}
+
+show_count = 50
+save_dir = "results/aes_eval/show_demo"
+os.makedirs(save_dir, exist_ok=True)
+
+if __name__ == "__main__":
+    with open(bench_json, 'r') as f:
+        bench_data = f.readlines()
+
+    random.shuffle(bench_data)
+    for line in bench_data:
+        data = json.loads(line)
+        image_path = os.path.join(data_dir, data['raw'])
+        raw_image = Image.open(image_path)
+        image_path = os.path.join(data_dir, data['target'])
+        target_image = Image.open(image_path)
+        instruction = data['instruction'] + f" (source: {data['source']})"
+        sample_type = data['type'].replace(" ", "_")
+        
+        result_images = {}
+        for tag, result_dir in result_dirs.items():
+            result_path = os.path.join(result_dir, data['target'].rsplit(".", 1)[0] + ".png")
+            if not os.path.exists(result_path):
+                continue
+            result_image = Image.open(result_path)
+            result_images[tag] = result_image
+        if len(result_images) != len(result_dirs):
+            continue
+        
+        # show the raw image, instruction, and target image in one raw
+        # and the result images in one raw
+        rows = 2
+        cols = max(2, len(result_images.keys()))
+        fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
+        axes[0, 0].imshow(raw_image)
+        axes[0, 0].set_title("Raw Image")
+        axes[0, 1].imshow(target_image)
+        axes[0, 1].set_title("Target Image")
+        fig.suptitle(instruction, wrap=True)
+        # axes[0, 1].set_title("Instruction")
+        # axes[0, 1].text(0.5, 0.5, instruction, ha='center', va='center')
+        for i, tag in enumerate(result_images.keys()):
+            axes[1, i].imshow(result_images[tag])
+            axes[1, i].set_title(f"{tag}")
+
+        save_path = os.path.join(save_dir, f"{sample_type}_{show_count:03d}.png")
+        plt.savefig(save_path, dpi=150, bbox_inches='tight')
+        plt.close()
+        show_count -= 1
+        print(f"Saved {save_path}")
+        
+        if show_count == 0:
+            break
